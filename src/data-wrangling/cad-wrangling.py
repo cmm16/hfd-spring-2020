b32df6624a212a1480fd7ba0bf95a635b35e27ab
cmm16 <cmm16@rice.edu>
2020-02-09 15:22:55 -0600

    Clean merge_csvs function and add comments for explain ability

--- src/data-wrangling/cad-wrangling.py
+++ src/data-wrangling/cad-wrangling.py
@@ -9,6 +9,7 @@ DATA = "/Users/work/PycharmProjects/hfd-spring-2020/data/"
 zip_cad_path = path.join(DATA, "UPDATE CAD DATA/")
 path.join(DATA, "unzip_cad")
 
+
 def unzip_folders(target_directory, save_directory):
     """
     unzips all the zip folders in a specified directory and stores all the unzipped files in a new directory
@@ -33,12 +34,17 @@ def merge_csvs(directory):
     Returns:
 
     """
+    # place holder array
     ar = np.ndarray((0, 6))
     for file in listdir(directory):
+        # logic statement to differentiate incident and incident unit
         if file.startswith("inc_unit"):
             path_to_csv = path.join(directory, file)
-            print(file, df.columns)
-            df = pd.read_csv(path_to_csv, skiprows = 1)
-            df_len = len(df.values)
-            df_ar = np.append(np.delete(df.values, [len(df.values) - 1, len(df.values) -2], axis = 0), np.reshape(df.columns, (1,6)), axis = 0)
+            # skip first line since this is sql garbage, will make first actual row into column names
+            df = pd.read_csv(path_to_csv, skiprows=1)
+            # take df values and remove two last lines that are sql garbage
+            df_values = np.delete(df.values, [len(df.values)-1, len(df.values)-2], axis=0)
+            # add column names to values since these are actually the first valid row
+            df_ar = np.append(df_values, np.reshape(df.columns, (1,6)), axis=0)
             ar = np.append(ar, df_ar, axis=0)
+    return ar
