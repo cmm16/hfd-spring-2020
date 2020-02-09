0b7ff43131a79b7b66f34afa4e9d3f78679f0e3c
cmm16 <cmm16@rice.edu>
2020-02-09 15:10:24 -0600

    Create new function merge_csvs

--- src/data-wrangling/cad-wrangling.py
+++ src/data-wrangling/cad-wrangling.py
@@ -21,3 +21,22 @@ def unzip_folders(directory):
             path_to_zip = path.join(zip_cad_path, file)
             with ZipFile(path_to_zip, 'r') as zipObj:
                 zipObj.extractall(path.join(DATA, "unzip_cad"))
+
+
+def merge_csvs(directory):
+    """
+
+    Arguments:
+
+    Returns:
+
+    """
+    ar = np.ndarray((0, 6))
+    for file in listdir(directory):
+        if file.startswith("inc_unit"):
+            path_to_csv = path.join(directory, file)
+            print(file, df.columns)
+            df = pd.read_csv(path_to_csv, skiprows = 1)
+            df_len = len(df.values)
+            df_ar = np.append(np.delete(df.values, [len(df.values) - 1, len(df.values) -2], axis = 0), np.reshape(df.columns, (1,6)), axis = 0)
+            ar = np.append(ar, df_ar, axis=0)
