fcd2a07deb2a127f95ae5f4e8c4a92c6234250b3
cmm16 <cmm16@rice.edu>
2020-02-09 15:38:08 -0600

    Add save_clean function

--- src/data-wrangling/cad-wrangling.py
+++ src/data-wrangling/cad-wrangling.py
@@ -51,6 +51,17 @@ def merge_csvs(directory):
             ar = np.append(ar, df_ar, axis=0)
     return ar
 
+
 def clean_inc_unit(ar):
+    merged_df = pd.DataFrame(ar)
+    # split the two columns that have space as their delimiter instead of comma
+    split_df1 = merged_df[4].str[1:-1].str.split(" ", expand=True)
+    split_df2 = merged_df[5].str[1:-1].str.split(" ", expand=True)
+    final_merged_df = pd.concat([merged_df[[0, 1, 2]], split_df1, split_df2], axis=1)
+    return final_merged_df.values()
+
+
+def save_clean(directory, ar):
+    np.savetxt(directory, ar, delimiter=',', fmt='%d')
 
 
