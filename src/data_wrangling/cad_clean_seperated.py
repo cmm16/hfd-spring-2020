11de0f1157878e60dc44e5a8ffa1fca2f8f7e901
Emre Yurtbay <eay2@rice.edu>
2020-02-10 15:20:10 -0600

    Added a new feature - transit_time. Some strange values, like negative transit times or extra large values exist. Runtime of script is about 5 minutes

--- src/data_wrangling/cad_clean_seperated.py
+++ src/data_wrangling/cad_clean_seperated.py
@@ -1,6 +1,36 @@
 import pandas as pd
 from os import path
 from os import getcwd
+import time
+
+
+def transit_calc(row):
+    """
+    Assumptions - Arrival time is always after departure time
+    """
+    # Most case: Departure time is 2:58 and Arrival Time is 4:01
+    if int(row['Dep_Hour']) <= int(row['Arr_Hour']):
+        diff = int(row['Arr_Hour']) - int(row['Dep_Hour'])
+
+        if diff == 0:
+            # For Instance, Departure time was 3:28 and Arrival Time was 3:58
+            val = int(row['Arr_Minute']) - int(row['Dep_Minute'])
+
+        elif diff == 1:
+            # For Instance, Departure time was 3:50 and Arrival Time was 4:01
+            val = 60 - (int(row['Dep_Minute'])) + int(row['Arr_Minute'])
+
+        else:
+            # For instance, Departure time was 3:59 and Arrival Time was 5:03
+            val = 60 - (int(row['Dep_Minute'])) + int(row['Arr_Minute']) + 60 * (diff - 1)
+
+    # Pathological Case : Departure time is at 23:50 and Arrival time is 00:30
+    else:
+
+        time_until_midnight = 60 * (24 - int(row['Dep_Hour']) - 1) + 60 - int(row['Dep_Minute'])
+        time_after_midnight = 60 * int(row['Arr_Hour']) + int(row['Arr_Minute'])
+        val = time_until_midnight + time_after_midnight
+    return val
 
 
 def format_date_time(csv_file):
@@ -41,6 +71,10 @@ def format_date_time(csv_file):
     dat = dat.drop('6', 1)
     dat = dat.drop('Unnamed: 0', 1)  # Some index column
 
+    # Calculate Transit Times - runtime is about 4 minutes
+    dat.is_copy = False  # pd.options.mode.chained_assignment = None is another option
+    dat['Transit_Time'] = dat.apply(transit_calc, axis=1)
+
     return dat
 
 
@@ -54,7 +88,7 @@ def save_formatted_cad_data(df, directory):
 
 
 def main():
-
+    # Runtime of main() is --- 290.41543197631836 seconds ---
     # Get into your Data Director
     data_dir = path.join(path.dirname(path.dirname(getcwd())), "data")
 
@@ -70,4 +104,6 @@ def main():
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
