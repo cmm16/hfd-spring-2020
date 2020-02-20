234370a69b1ec3a2f1659f02291b588c9423c5e4
cmm16 <cmm16@rice.edu>
2020-02-20 09:30:32 -0600

    Add aggregate.py to main.py

--- src/data_wrangling/main.py
+++ src/data_wrangling/main.py
@@ -1,10 +1,12 @@
 from src.data_wrangling.cad_wrangling import cad_wrangling
 from src.data_wrangling.spatial_join import spatial_join
+from src.data_wrangling.aggregate import aggregate
 
 
 def main():
     cad_wrangling()
     spatial_join()
+    aggregate()
 
 
 if __name__ == "__main__":
