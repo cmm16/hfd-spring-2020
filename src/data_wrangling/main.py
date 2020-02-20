473bc571c7fa5149c1472c8836e917900085eafc
cmm16 <cmm16@rice.edu>
2020-02-20 12:53:08 -0600

    Reformat main.py and rework sub functions to work with new structuring

--- src/data_wrangling/main.py
+++ src/data_wrangling/main.py
@@ -1,12 +1,19 @@
+from os import path
+from os import getcwd
 from src.data_wrangling.cad_wrangling import cad_wrangling
 from src.data_wrangling.spatial_join import spatial_join
 from src.data_wrangling.aggregate import aggregate
 
 
 def main():
-    cad_wrangling()
-    spatial_join()
-    aggregate()
+    data_dir = path.join(path.dirname(path.dirname(getcwd())), "data")
+    inc_save_path = path.join(data_dir, "inc_cad_clean.csv")
+    # unit_save_path = path.join(data_dir, "unit_cad_clean.csv")
+
+    cad_wrangling(data_dir, inc_save_path, inc_type="inc", unzip=True)
+    # cad_wrangling()
+    # spatial_join()
+    # aggregate()
 
 
 if __name__ == "__main__":
