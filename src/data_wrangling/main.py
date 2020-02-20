2123727faedb2502f346e43c0c804f205e0598b8
cmm16 <cmm16@rice.edu>
2020-02-20 13:04:40 -0600

    Reformat main.py and rework sub functions in spatial join to work with new structuring

--- src/data_wrangling/main.py
+++ src/data_wrangling/main.py
@@ -6,13 +6,24 @@ from src.data_wrangling.aggregate import aggregate
 
 
 def main():
+    # set data directory to base plus /data
     data_dir = path.join(path.dirname(path.dirname(getcwd())), "data")
-    inc_save_path = path.join(data_dir, "inc_cad_clean.csv")
-    # unit_save_path = path.join(data_dir, "unit_cad_clean.csv")
 
+    # unzip merge and clean csvs saving to save path location
+    # for incidents csvs
+    inc_save_path = path.join(data_dir, "inc_cad_clean.csv")
     cad_wrangling(data_dir, inc_save_path, inc_type="inc", unzip=True)
-    # cad_wrangling()
-    # spatial_join()
+    # for unit csvs
+    unit_save_path = path.join(data_dir, "unit_cad_clean.csv")
+    cad_wrangling(data_dir, unit_save_path, inc_type="unit", unzip=True)
+
+    # performs spatial join on geojson data and points data saving to save path location
+    geojson_data_path = path.join(data_dir, "/Uploaded_Shapefiles/CensusBlock_2010/censusblock_2010_clip_by_fir.geojson")
+    points_data_path = path.join(data_dir, "/inc_cad_clean.csv")
+    geo_join_save_path = path.join(data_dir, "data/inc_cad_clean_with_loc.csv")
+    spatial_join(geojson_data_path, points_data_path, geo_join_save_path)
+
+
     # aggregate()
 
 
