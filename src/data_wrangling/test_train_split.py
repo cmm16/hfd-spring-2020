e967ea6f74d5f54b2af2494a0e6aa784ef1ec67f
cmm16 <cmm16@rice.edu>
2020-02-20 16:54:35 -0600

    Add test_train_split to main

--- src/data_wrangling/test_train_split.py
+++ src/data_wrangling/test_train_split.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-def test_train_split(input_data_path, save_path, test_percent=.2):
+def test_train_split(input_data_path, train_save_path, test_save_path, test_percent=.2):
     """
     Splits input data into a train and test set and saves each separately
 
@@ -13,6 +13,7 @@ def test_train_split(input_data_path, save_path, test_percent=.2):
     """
 
     df = pd.read_csv(input_data_path)
-    train, test = train_test_split(df, test_size=test_percent)
-    train.to_csv(save_path + "_train")
-    test.to_csv(save_path + "_test")
+    train, test = train_test_split(df, test_size=test_percent, random_state=17)
+    print(type(train))
+    # train.to_csv(train_save_path)
+    # test.to_csv(test_save_path)
