1f8032bef603c3d98723381e82f7d91ec05223ba
cmm16 <cmm16@rice.edu>
2020-02-20 19:31:58 -0600

    Rename function inputs

--- src/data_wrangling/main.py
+++ src/data_wrangling/main.py
@@ -4,6 +4,7 @@ from src.data_wrangling.cad_wrangling import cad_wrangling
 from src.data_wrangling.spatial_join import spatial_join
 from src.data_wrangling.aggregate import aggregate
 from src.data_wrangling.test_train_split import test_train_split
+from src.data_wrangling.merge_on_bg import merge_by_bg
 
 
 def main():
@@ -12,24 +13,28 @@ def main():
     # unzip merge and clean csvs saving to save path location
     # for incidents csvs
     inc_save_path = path.join(data_dir, "inc_cad_clean.csv")
-    cad_wrangling(data_dir, inc_save_path, inc_type="inc", unzip=True)
+    #cad_wrangling(data_dir, inc_save_path, inc_type="inc", unzip=True)
     # for unit csvs
     unit_save_path = path.join(data_dir, "unit_cad_clean.csv")
-    cad_wrangling(data_dir, unit_save_path, inc_type="unit", unzip=True)
+    #cad_wrangling(data_dir, unit_save_path, inc_type="unit", unzip=True)
 
     # performs spatial join on geojson data and points data saving to save path location
     geojson_data_path = path.join(data_dir, "Uploaded_Shapefiles/CensusBlock_2010/censusblock_2010_clip_by_fir.geojson")
     geo_join_save_path = path.join(data_dir, "inc_cad_clean_with_loc.csv")
-    spatial_join(geojson_data_path, inc_save_path, geo_join_save_path)
+    #spatial_join(geojson_data_path, inc_save_path, geo_join_save_path)
 
     # performs group by on specified columns
     group_columns = ["Block_Group", "Call_Type"]
     groupby_save_path = path.join(data_dir, "bg_call_type_aggregate.csv")
     aggregate(geo_join_save_path, group_columns, groupby_save_path)
 
+    path_to_bg_data = path.join(data_dir, "American_Community_Survey_(ACS)_2017_Harris/Curated/2017/ACS20175Harris_BG_v01.csv")
+    census_save_path = path.join(data_dir, "census_merged.csv")
+    merge_by_bg(groupby_save_path, path_to_bg_data, "Block_Group", "GeoID17bg", census_save_path)
+
     train_save_path = path.join(data_dir, "bg_inc_train.csv")
     test_save_path = path.join(data_dir, "bg_inc_test.csv")
-    test_train_split(groupby_save_path, train_save_path, test_save_path)
+    test_train_split(census_save_path, train_save_path, test_save_path)
 
 
 if __name__ == "__main__":
--- src/data_wrangling/merge_on_bg.py
+++ src/data_wrangling/merge_on_bg.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 
-def merge_by_bg(path_to_merge_data, path_to_bg_data, merge_column_name, save_path):
+def merge_by_bg(path_to_data1, path_to_data2, merge_column_name1, merge_column_name2, save_path):
     """
     Merges two data frames with common bg key column
 
-    path_to_merge_data (str): String path data to be merged
-    path_to_bg_data (str): String path to block group data
+    path_to_data1 (str): String path data to be merged
+    path_to_data2 (str): String path to block group data
     merge_column_name (str): String name of column to merge on in merge data
     save_path (str): String path to desired save location
     """
-    new_df = pd.read_csv(path_to_merge_data)
-    bg_df = pd.read_csv(path_to_bg_data)
-    merged_df = bg_df.merge(new_df, left_on="Block_Group", right_on=merge_column_name).to_csv(save_path)
+    df1 = pd.read_csv(path_to_data1)
+    df2 = pd.read_csv(path_to_data2)
+    print(df1.head())
+    merged_df = df1.merge(df2, left_on=merge_column_name1, right_on=merge_column_name2).to_csv(save_path)
     merged_df.to_csv(save_path)
 
