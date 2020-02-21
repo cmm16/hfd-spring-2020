ef28bbee10bcf970eab6e0390d6f920ee8a80214
cmm16 <cmm16@rice.edu>
2020-02-20 19:36:52 -0600

    Run black

--- src/data_wrangling/merge_on_bg.py
+++ src/data_wrangling/merge_on_bg.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def merge_by_bg(path_to_data1, path_to_data2, merge_column_name1, merge_column_name2, save_path):
+def merge_by_bg(
+    path_to_data1, path_to_data2, merge_column_name1, merge_column_name2, save_path
+):
     """
     Merges two data frames with common bg key column
 
@@ -14,4 +16,3 @@ def merge_by_bg(path_to_data1, path_to_data2, merge_column_name1, merge_column_n
     df2 = pd.read_csv(path_to_data2)
     merged_df = df1.merge(df2, left_on=merge_column_name1, right_on=merge_column_name2)
     merged_df.to_csv(save_path, index=False)
-
