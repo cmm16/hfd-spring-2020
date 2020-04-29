d6dfa3c23d0fbb55c9b6e2362fbd163e16b132b4
Cole Morgan <cmm16@rice.edu>
2020-04-28 21:27:49 -0500

    Add target viz function

--- src/modeling/model_visualizations.py
+++ src/modeling/model_visualizations.py
@@ -1,2 +1,18 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import shap
+
+
+def visualize_targets(y_data):
+    """
+    Creates visuals that show the univariate and bivariate relationships of the variables
+
+    Arguments:
+        y_data (DataFrame):
+    """
+    sns.pairplot(data=y_data)
+    for col in y_data.columns:
+        # add titles
+        sns.swarmplot(col, data=y_data)
+        plt.show()
+
