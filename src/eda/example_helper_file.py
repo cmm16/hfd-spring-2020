e954b22d5ca5b70331f653640bce5fb905a680ed
cmm16 <cmm16@rice.edu>
2020-05-04 21:30:54 -0500

    add example helper file

---
+++ src/eda/example_helper_file.py
@@ -0,0 +1,5 @@
+# this file could for example run histograms or race ed
+
+
+def run_race_eda():
+    pass
--- src/eda/main_eda.py
+++ src/eda/main_eda.py
@@ -1,9 +1,13 @@
 from os.path import join, dirname
 from os import getcwd
+# you should import all helper files here
+from src.eda.example_helper_file import run_race_eda
+# each helper file should have one or two functions that you call here those
+
 
 def main(data_dir):
     # data dir will be set to any users Data directory if they put it at the top level of the project
-
+    run_race_eda()
     print("hello")
 
 if __name__ == '__main__':
