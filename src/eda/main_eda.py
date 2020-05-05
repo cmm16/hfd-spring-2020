56bd2606d10463f06417cd2825e2a2d4216babe7
cmm16 <cmm16@rice.edu>
2020-05-04 21:32:56 -0500

    add examples

--- src/eda/main_eda.py
+++ src/eda/main_eda.py
@@ -7,8 +7,15 @@ from src.eda.example_helper_file import run_race_eda
 
 def main(data_dir):
     # data dir will be set to any users Data directory if they put it at the top level of the project
+
+
     run_race_eda()
     print("hello")
+
+    # I can use data dir arguement to access any piece of data I want by using join
+    print("data dir", data_dir)
+    #path_to_some_data = join(data_dir, "some_data")
+
 
 if __name__ == '__main__':
     main(join(dirname(dirname(getcwd())), "Data"))
