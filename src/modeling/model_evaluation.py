09a25c32ed00d20fd75159cbd3ff2c62bb8b8140
Cole Morgan <cmm16@rice.edu>
2020-05-05 18:47:37 -0500

    clean model evaluation

--- src/modeling/model_evaluation.py
+++ src/modeling/model_evaluation.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import r2_score
 
 
 def compute_error_metrics(X_train, X_test, y_train, y_test, models):
     train_prediction_df = pd.DataFrame(y_train.index).set_index("Block_Group")
     test_prediction_df = pd.DataFrame(y_test.index).set_index("Block_Group")
 
+    # used each model to predict X_train and X_test
     for i, col in enumerate(y_train):
         c_model = models[i]
         c_preds = []
@@ -20,36 +20,33 @@ def compute_error_metrics(X_train, X_test, y_train, y_test, models):
             c_preds.append(s_c_model.predict(X_test))
         test_prediction_df["Predicted " + col] = np.mean(c_preds, 0)
 
+    # create df of all predictions
     all_predictions_df = test_prediction_df.append(train_prediction_df)
     all_actuals_df = y_test.append(y_train)
 
+    # create error metric data frame
     error_df = pd.DataFrame(zip(all_actuals_df.mean(), all_actuals_df.std()),
                             columns=["Mean", "Std"], index=all_actuals_df.columns)
     train_mae = []
     train_rsme = []
-    train_r2_scores = []
-
     test_mae = []
     test_rsme = []
-    test_r2_scores = []
 
+    # compute error metrics per model
     for i, col in enumerate(y_train.columns):
         y_cur = y_train[col]
         y_pred_cur = train_prediction_df.iloc[:, i]
         train_mae.append(np.mean(np.abs(y_cur - y_pred_cur)))
         train_rsme.append(np.sqrt(np.mean(np.square(y_cur - y_pred_cur))))
-        train_r2_scores.append(r2_score(y_cur, y_pred_cur))
     for i, col in enumerate(y_train):
         y_cur = y_test[col]
         y_pred_cur = test_prediction_df.iloc[:, i]
         test_mae.append(np.mean(np.abs(y_cur - y_pred_cur)))
         test_rsme.append(np.sqrt(np.mean(np.square(y_cur - y_pred_cur))))
-        test_r2_scores.append(r2_score(y_cur, y_pred_cur))
 
-    #error_df["Train R^2"] = train_r2_scores
+    # add computed error metrics to error metric data frame
     error_df["Train MAE"] = train_mae
     error_df["Train RMSE"] = train_rsme
-    #error_df["Test R^2"] = test_r2_scores
     error_df["Test MAE"] = test_mae
     error_df["Test RMSE"] = test_rsme
     error_df.append([error_df[col].mean() for col in error_df.columns])
