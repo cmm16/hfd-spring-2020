b7c7e75cc96d2c22a893b8e82adff213de4cfa48
Cole Morgan <cmm16@rice.edu>
2020-05-04 21:02:37 -0500

    Modify wrangling to work with modeling

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from src.data_wrangling import main_wrangling
-from os import path
+from os.path import join, dirname
 from os import getcwd
 
 
-def main(data_dir=path.join((path.dirname(getcwd())), "data")):
+def main(data_dir=join((dirname(getcwd())), "Data")):
     main_wrangling.main(data_dir)
 
 
