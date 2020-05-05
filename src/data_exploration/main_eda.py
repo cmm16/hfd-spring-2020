8e985a81f994ee98866c92a8191e1d2be69a2d73
Melinda Ding <mjd4@rice.edu>
2020-05-04 23:51:47 -0500

    Finish time series EDA

--- src/data_exploration/main_eda.py
+++ src/data_exploration/main_eda.py
@@ -1,21 +1,25 @@
 from os.path import join, dirname
-from os import getcwd
+import pandas as pd
+from os import getcwd, mkdir
 # you should import all helper files here
-from src.eda.example_helper_file import run_race_eda
+import time_series as time_eda
 # each helper file should have one or two functions that you call here those
 
+incidents_df = pd.read_csv("test/imagetrend_incidents.csv")
 
-def main(data_dir):
+def main(output_dir):
+    mkdir(output_dir)
+
     # data dir will be set to any users Data directory if they put it at the top level of the project
+    time_eda.run_time_eda(output_dir, incidents_df) 
 
-
-    run_race_eda()
+    # run_race_eda()
     print("hello")
 
     # I can use data dir arguement to access any piece of data I want by using join
-    print("data dir", data_dir)
+    print("data dir:", output_dir)
     #path_to_some_data = join(data_dir, "some_data")
 
 
 if __name__ == '__main__':
-    main(join(dirname(dirname(getcwd())), "Data"))
+    main(join(dirname(dirname(getcwd())), "eda_output"))
