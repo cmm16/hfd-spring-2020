9ccfbaa29394db86ced5c8ce01aff85ea23889ce
Cole Morgan <cmm16@rice.edu>
2020-05-05 19:55:37 -0500

    run black on time parsing

--- src/data_wrangling/time_parsing.py
+++ src/data_wrangling/time_parsing.py
@@ -1,5 +1,5 @@
 def parseDates(df):
-	"""
+    """
 	Parses an incident id into 
 
 	Input: a data frame with incident id where incident id format 
@@ -8,11 +8,11 @@ def parseDates(df):
 	Returns: the same data frame with 4 new columns titled Year, Month, Date, 
 		Incident. 
 	"""
-	df["inc_string"] = df["Incident_ID"].apply(str)
+    df["inc_string"] = df["Incident_ID"].apply(str)
 
-	# Incident ID format = YYMMDDseqx
-	df["Year"] = ("20" + df["inc_string"].str[:2]).astype(int)
-	df["Month"] = df["inc_string"].str[2:4].astype(int)
-	df["Date"] = df["inc_string"].str[4:6].astype(int)
-	df["Incident"] = df["inc_string"].str[6:11].astype(int)
-	return df
+    # Incident ID format = YYMMDDseqx
+    df["Year"] = ("20" + df["inc_string"].str[:2]).astype(int)
+    df["Month"] = df["inc_string"].str[2:4].astype(int)
+    df["Date"] = df["inc_string"].str[4:6].astype(int)
+    df["Incident"] = df["inc_string"].str[6:11].astype(int)
+    return df
