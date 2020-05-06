ef08a570c8bb1314bb71d1cb319bbe5b8cab9285
Cole Morgan <cmm16@rice.edu>
2020-05-06 02:56:56 -0500

    update code

--- src/data_exploration/time_eda.py
+++ src/data_exploration/time_eda.py
@@ -80,26 +80,29 @@ def plot_decomposition(output_dir, timedf):
     plt.savefig(join(output_dir, "timeseries_decomposition.png"))
 
 def plot_yearly_dist(output_dir, incidents_df): 
-	"""
-	Creates stacked bar chart of call distribution for each year.
+    """
+    Creates stacked bar chart of call distribution for each year.
 
-	Input:
-		- output_dir: String path to data directory
-		- incidents_df: Dataframe of incidents that must include parsed time and call category
-	"""
-	fig = plt.figure(figsize=(15,10))
-	ax = fig.add_subplot(111)
-	years = incidents_df.groupby(["Year", "Call_Category"]).Event_Number.count().unstack(1)
-	callsPercent = years.apply(lambda row: row/row.sum(), axis=1)
-	callsPercent.plot(ax=ax,kind='bar', stacked=True, rot=0, color=['coral', 'red', 'yellow', 'darkorange', 'firebrick', 'gold'])
-	mylabels = ["Health (internal)", "External Injuries", "Mental Illness", "Motor", 'Fire', 'Other']
-	plt.title("Yearly Call Distribution", fontsize=20)
-	plt.ylabel("Portion of Calls", fontsize=18)
-	plt.yticks(fontsize=15)
-	plt.xlabel("Year", fontsize=18)
-	plt.xticks(fontsize=15)
-	plt.legend(labels = mylabels)
-	plt.savefig(join(output_dir, "yearly_call_distribution.png"))
+    Input:
+    - output_dir: String path to data directory
+    - incidents_df: Dataframe of incidents that must include parsed time and call category
+    """
+    fig = plt.figure(figsize=(15,10))
+    ax = fig.add_subplot(111)
+    incidents_df["Subcall_Code"] = incidents_df["Event_Type"].str[:4]
+    incidents_df["Call_Category"] = incidents_df["Subcall_Code"].apply(
+    lambda x: call_category_map[x])
+    years = incidents_df.groupby(["Year", "Call_Category"]).Event_Number.count().unstack(1)
+    callsPercent = years.apply(lambda row: row/row.sum(), axis=1)
+    callsPercent.plot(ax=ax,kind='bar', stacked=True, rot=0, color=['coral', 'red', 'yellow', 'darkorange', 'firebrick', 'gold'])
+    mylabels = ["Health (internal)", "External Injuries", "Mental Illness", "Motor", 'Fire', 'Other']
+    plt.title("Yearly Call Distribution", fontsize=20)
+    plt.ylabel("Portion of Calls", fontsize=18)
+    plt.yticks(fontsize=15)
+    plt.xlabel("Year", fontsize=18)
+    plt.xticks(fontsize=15)
+    plt.legend(labels = mylabels)
+    plt.savefig(join(output_dir, "yearly_call_distribution.png"))
 
 
 call_category_map = {
