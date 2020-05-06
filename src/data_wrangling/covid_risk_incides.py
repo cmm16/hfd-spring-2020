5de52d0214733e94aeb4163c80eda29bae4fb5a0
Cole Morgan <cmm16@rice.edu>
2020-05-05 20:09:39 -0500

    run black and optimize imports on covid_risk_incides.py

--- src/data_wrangling/covid_risk_incides.py
+++ src/data_wrangling/covid_risk_incides.py
@@ -1,49 +1,93 @@
-from scipy.stats.mstats import gmean
+from os.path import join
+
 import numpy as np
-from os.path import join, dirname
 import pandas as pd
-from os import getcwd
+from scipy.stats.mstats import gmean
+
 
 class CovidRiskCalculator:
     def __init__(self, dat, call_counts, data_dir, save_path):
-        dat['pct_elderly'] = dat["pctAdult4564"] + dat['pctAge65p']
-        dat['health_pc'] = dat["health"] / dat['TotalPop']
+        dat["pct_elderly"] = dat["pctAdult4564"] + dat["pctAge65p"]
+        dat["health_pc"] = dat["health"] / dat["TotalPop"]
 
         file2 = "percent_uninsured_block_group.csv"
         data_file2 = join(data_dir, file2)
 
         insurance = pd.read_csv(data_file2)
-        insurance = insurance.rename(columns={"FIPS": "Block_Group",
-                                              "% FINANCIAL SERVICES | HEALTH/LIFE INSURANCE | HEALTH/HOSPITAL/LIFE INSR-CURRENTLY HAVE | NO, 2019": "insurance"})
+        insurance = insurance.rename(
+            columns={
+                "FIPS": "Block_Group",
+                "% FINANCIAL SERVICES | HEALTH/LIFE INSURANCE | HEALTH/HOSPITAL/LIFE INSR-CURRENTLY HAVE | NO, 2019": "insurance",
+            }
+        )
 
         dat = dat.merge(insurance, on="Block_Group")
         dat = dat.fillna(dat.median())
-        dat["Call Percent"] = call_counts.sum(1)/ sum(call_counts.sum(1))
-        for col in ["fire", "health", "injuries_external", "mental_illness", "motor", "other"]:
+        dat["Call Percent"] = call_counts.sum(1) / sum(call_counts.sum(1))
+        for col in [
+            "fire",
+            "health",
+            "injuries_external",
+            "mental_illness",
+            "motor",
+            "other",
+        ]:
             dat["percent " + col] = call_counts[col] / sum(call_counts.sum(1))
 
         self.dat = dat
         self.save_path = save_path
 
-
     def create_covid_df(self):
-        self.dat['Poverty_Index'] = self.dat.apply(self.calculatePovertyIndex, axis=1)
-        self.dat['Diversity_Index'] = self.dat.apply(self.calculateDiversityIndex, axis=1)
-        self.dat['Health_Affliction_Index'] = self.dat.apply(self.calculateHealthAfflictionIndex, axis=1)
+        self.dat["Poverty_Index"] = self.dat.apply(self.calculatePovertyIndex, axis=1)
+        self.dat["Diversity_Index"] = self.dat.apply(
+            self.calculateDiversityIndex, axis=1
+        )
+        self.dat["Health_Affliction_Index"] = self.dat.apply(
+            self.calculateHealthAfflictionIndex, axis=1
+        )
 
         # Normalize the Indicies to be 0-1
-        self.dat['Health_Affliction_Index'] = ((self.dat['Health_Affliction_Index'] - self.dat['Health_Affliction_Index'].min())/(self.dat['Health_Affliction_Index'].max()-self.dat['Health_Affliction_Index'].min()))
-        self.dat['Poverty_Index'] = ((self.dat['Poverty_Index'] - self.dat['Poverty_Index'].min())/(self.dat['Poverty_Index'].max()-self.dat['Poverty_Index'].min()))
-        self.dat['Diversity_Index'] = ((self.dat['Diversity_Index'] - self.dat['Diversity_Index'].min())/(self.dat['Diversity_Index'].max()-self.dat['Diversity_Index'].min()))
+        self.dat["Health_Affliction_Index"] = (
+            self.dat["Health_Affliction_Index"]
+            - self.dat["Health_Affliction_Index"].min()
+        ) / (
+            self.dat["Health_Affliction_Index"].max()
+            - self.dat["Health_Affliction_Index"].min()
+        )
+        self.dat["Poverty_Index"] = (
+            self.dat["Poverty_Index"] - self.dat["Poverty_Index"].min()
+        ) / (self.dat["Poverty_Index"].max() - self.dat["Poverty_Index"].min())
+        self.dat["Diversity_Index"] = (
+            self.dat["Diversity_Index"] - self.dat["Diversity_Index"].min()
+        ) / (self.dat["Diversity_Index"].max() - self.dat["Diversity_Index"].min())
 
-        self.dat['Risk_Index'] = self.dat.apply(self.calculateRiskIndex, axis=1)
+        self.dat["Risk_Index"] = self.dat.apply(self.calculateRiskIndex, axis=1)
 
-        indexDF = self.dat[["Block_Group", "Health_Affliction_Index", 'Poverty_Index','Diversity_Index', "Risk_Index", "Call Percent"] + ["percent " + col for col in ["fire", "health", "injuries_external", "mental_illness", "motor", "other"]]]
+        indexDF = self.dat[
+            [
+                "Block_Group",
+                "Health_Affliction_Index",
+                "Poverty_Index",
+                "Diversity_Index",
+                "Risk_Index",
+                "Call Percent",
+            ]
+            + [
+                "percent " + col
+                for col in [
+                    "fire",
+                    "health",
+                    "injuries_external",
+                    "mental_illness",
+                    "motor",
+                    "other",
+                ]
+            ]
+        ]
         indexDF.to_csv(self.save_path, index=False)
 
-
     def calculatePovertyIndex(self, row):
-        '''
+        """
          Factors considered come from the neighborhood deprivation index
         "pctFemHHwc" : % Single Mothers, higher = poorer
         "pct_ltHS": % Of people without high school diploma, higher = poorer
@@ -55,15 +99,22 @@ class CovidRiskCalculator:
         "pct_gt1": A crowededness index, higher = poorer
 
         A high poverty index (closer to 1) indicates a poorer block group
-        '''
+        """
         # print("____________________________________________")
         # print("Block Group")
         # print(row["Block_Group"])
         # print("____________________________________________")
 
         # List of Columns that go into the Prosperity Index
-        criteriaHigh = ["pctFemHHwc", "pct_ltHS", "pct_lt30", "pctPubAsst", "pctBelow", "pct_gt1"]
-        criteriaLow = ['pct_Prof_M', 'pct_Prof_F']
+        criteriaHigh = [
+            "pctFemHHwc",
+            "pct_ltHS",
+            "pct_lt30",
+            "pctPubAsst",
+            "pctBelow",
+            "pct_gt1",
+        ]
+        criteriaLow = ["pct_Prof_M", "pct_Prof_F"]
 
         # List of indiviudal Indicies (0, 1) for each column
         indicies = []
@@ -86,7 +137,7 @@ class CovidRiskCalculator:
         return poverty_index
 
     def calculateDiversityIndex(self, row):
-        '''
+        """
         Factors to calculate a neighborhood diversity index
         pctNHwht: Percentage white block group
 
@@ -94,7 +145,7 @@ class CovidRiskCalculator:
         indicates a high number of Black or hispanic or other
         people
         in a block group
-        '''
+        """
         # criteria = ["pctHisp", "pctNHblk", "pctNHoth"]
         criteria = ["pctNHwht"]
 
@@ -114,7 +165,7 @@ class CovidRiskCalculator:
         return diversity_index
 
     def calculateHealthAfflictionIndex(self, row):
-        '''
+        """
         Factors to calculate a Health Affliction index
         pct_elderly: Percent of People in age range 45+
         health_pc : health related calls per capita
@@ -123,7 +174,7 @@ class CovidRiskCalculator:
 
         A high Health Affliction index (closer to 1)
         indicates more potential for sickness in a block group
-        '''
+        """
         criteria = ["pct_elderly", "health_pc", "insurance"]
 
         # List of indiviudal Indicies (0, 1) for each column
@@ -141,15 +192,15 @@ class CovidRiskCalculator:
         return health_affliction_index
 
     def calculateRiskIndex(self, row):
-        '''
+        """
         Factors to calculate a Risk index
 
 
 
         A high Health Affliction index (closer to 1)
         indicates more potential for sickness in a block group
-        '''
-        criteria = ["Health_Affliction_Index", 'Poverty_Index', 'Diversity_Index']
+        """
+        criteria = ["Health_Affliction_Index", "Poverty_Index", "Diversity_Index"]
 
         # List of indiviudal Indicies (0, 1) for each column
         indicies = []
@@ -164,4 +215,3 @@ class CovidRiskCalculator:
         # risk = np.mean(indicies)
 
         return risk
-
