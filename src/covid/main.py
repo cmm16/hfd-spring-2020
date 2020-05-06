be24966d7ac4e7001cd1a97cafabcaa91f51ae18
Cole Morgan <cmm16@rice.edu>
2020-05-06 01:12:48 -0500

    run blakc and optimize imports im main for covid

--- src/covid/main.py
+++ src/covid/main.py
@@ -1,24 +1,27 @@
-from os.path import join, dirname
 from os import getcwd, mkdir
-import pandas as pd 
+from os.path import join, dirname
 
-import time_eda
-import clustering
-import call_prob
-import final_labels
+import pandas as pd
 
-### Sample file paths ### 
-incidents =  pd.read_csv("test/imagetrend_incidents.csv")
+import src.covid.call_prob as call_prob
+import src.covid.clustering as clustering
+import src.covid.final_labels as final_labels
+import src.covid.time_eda as time_eda
+
+### Sample file paths ###
+incidents = pd.read_csv("test/imagetrend_incidents.csv")
 data = pd.read_csv("test/covid_indices.csv")
-bg_filepath = 'test/Census_FIP12_within_Fire_Dis.geojson'
+bg_filepath = "test/Census_FIP12_within_Fire_Dis.geojson"
+
 
 def main(output_dir):
-	mkdir(output_dir)
+    mkdir(output_dir)
 
-	time_eda.run(output_dir, incidents)
-	clustered_df = clustering.run(output_dir, data, bg_filepath)
-	all_labelled = call_prob.run(output_dir, clustered_df)
-	final_labels.run(output_dir, all_labelled, bg_filepath)
+    time_eda.run(output_dir, incidents)
+    clustered_df = clustering.run(output_dir, data, bg_filepath)
+    all_labelled = call_prob.run(output_dir, clustered_df)
+    final_labels.run(output_dir, all_labelled, bg_filepath)
+
 
 if __name__ == "__main__":
     main(join(dirname(dirname(getcwd())), "covid_output"))
