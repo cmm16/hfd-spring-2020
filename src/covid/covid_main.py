7c520c23dc9a2c2d4b0cabb50995fb9176e25e6a
mel-ding <34950826+mel-ding@users.noreply.github.com>
2020-05-06 14:09:01 -0500

    Update covid_main.py

--- src/covid/covid_main.py
+++ src/covid/covid_main.py
@@ -8,6 +8,7 @@ import src.covid.call_prob as call_prob
 import src.covid.clustering as clustering
 import src.covid.final_labels as final_labels
 import src.covid.time_eda as time_eda
+import src.covid.fire_dist as fire_dist
 
 
 def main(data_dir):
@@ -25,11 +26,13 @@ def main(data_dir):
         join(join(data_dir, "Uploaded_Shapefiles"), "CensusBlock_2010"),
         "Census_FIP12_within_Fire_Dis.geojson",
     )
-
+    ### BLOCK GROUP FUNCTIONS ### 
     time_eda.run(output_dir, incidents)
     clustered_df = clustering.run(output_dir, data, bg_filepath)
     all_labelled = call_prob.run(output_dir, clustered_df)
     final_labels.run(output_dir, all_labelled, bg_filepath)
+    ### FIRE DISTRICT FUNCTIONS ### 
+	fire_dist.run(output_dir, fd_data, fd_filepath)
 
 
 if __name__ == "__main__":
