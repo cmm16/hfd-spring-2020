aa9ac43f611cf1d9d581c29a438526d1c372da61
Cole Morgan <cmm16@rice.edu>
2020-05-05 20:17:38 -0500

    run black and optimize imports on airport_eda.py

--- src/data_exploration/airport_eda.py
+++ src/data_exploration/airport_eda.py
@@ -1,16 +1,14 @@
-import seaborn as sns
 from os.path import join
+
+import seaborn as sns
 
 
 def plot_per_capita_calls_by_block_group(df, title, output_dir, with_out_hobby):
-    df["total_cals"] = df[["fire",
-"health",
-'injuries_external',
-'mental_illness',
-'motor',
-'other']].sum(1)
+    df["total_cals"] = df[
+        ["fire", "health", "injuries_external", "mental_illness", "motor", "other"]
+    ].sum(1)
     df["call_per_cap"] = df["total_cals"] / df["TotalPop"]
-    sns.set(rc={"figure.figsize":(13,8)})
+    sns.set(rc={"figure.figsize": (13, 8)})
     if with_out_hobby:
         new_df = df.copy()
         new_df = new_df[new_df["Block_Group"] != 482019800001.0]
@@ -22,7 +20,14 @@ def plot_per_capita_calls_by_block_group(df, title, output_dir, with_out_hobby):
     sns_plot.figure.savefig(join(output_dir, title))
     sns_plot.figure.clf()
 
-def run_airports_eda(output_dir, df):
-    plot_per_capita_calls_by_block_group(df, "Swarm Plot of Calls per Capita by Block Group", output_dir, False)
-    plot_per_capita_calls_by_block_group(df, "Swarm Plot of Calls per Capita by Block Group without Bush", output_dir, True)
 
+def run_airports_eda(output_dir, df):
+    plot_per_capita_calls_by_block_group(
+        df, "Swarm Plot of Calls per Capita by Block Group", output_dir, False
+    )
+    plot_per_capita_calls_by_block_group(
+        df,
+        "Swarm Plot of Calls per Capita by Block Group without Bush",
+        output_dir,
+        True,
+    )
